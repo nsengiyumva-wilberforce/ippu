81e361c6fdf5cce582c637ec8a05592ad1e39827
wilber4c <nsengiyumvawilberforce@gmail.com>
2023-09-06 12:24:14 +0300

    jobs API update

---
+++ app/Http/Controllers/API/JobsController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $jobs = Job::whereDate('visible_from', '<=', date('Y-m-d'))
+            ->whereDate('visible_to', '>=', date('Y-m-d'))
+            ->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $jobs
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,23 +6,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\CpdsController;
 use App\Http\Controllers\API\AccountTypeController;
+use App\Http\Controllers\API\JobsController;
 
-
-
-
+//routes for authentication
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
     Route::post('logout', 'logout');
 });
 Route::apiResource('account-types', AccountTypeController::class)->only(['index', 'show']);
+Route::apiResource('education-background', EducationBackgroundController::class);
 
-//Cpds API resource routes with auth middleware
+//cpds routes
+Route::apiResource('cpds', CpdsController::class);
+Route::get('upcoming-cpds', [CpdsController::class, 'upcoming']);
+Route::get('attended-cpds', [CpdsController::class, 'attended']);
 
-    Route::apiResource('cpds', CpdsController::class);
-    Route::apiResource('events', EventController::class);
-    Route::apiResource('education-background', EducationBackgroundController::class);
-    Route::get('upcoming-cpds', [CpdsController::class, 'upcoming']);
-    Route::get('attended-cpds', [CpdsController::class, 'attended']);
-    Route::get('upcoming-events', [EventController::class, 'upcoming']);
-    Route::get('attended-events', [EventController::class, 'attended']);
+//events routes
+Route::get('upcoming-events', [EventController::class, 'upcoming']);
+Route::get('attended-events', [EventController::class, 'attended']);
+Route::apiResource('events', EventController::class);
+
+
+//jobs routes
+Route::apiResource('jobs', JobsController::class)->only(['index', 'show']);
