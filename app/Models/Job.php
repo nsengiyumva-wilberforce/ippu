ca774f3975859913d54bcdb4f949a2cdbd598ae9
wills4c <nwilberforce256@gmail.com>
2023-12-06 04:04:08 +0300

    job notifications

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\PushNotification;
 
 class Job extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Triggered when a new model is being created
+        static::creating(function ($model) {
+            $model->sendNotification();
+        });
+    }
+
+    public function sendNotification()
+    {
+        //get the user with id 1
+        $devices = UserFcmDeviceToken::all();
+
+        //send notification to users
+        Notification::send($devices, new PushNotification("default.png", $this->title));
+    }
 }
